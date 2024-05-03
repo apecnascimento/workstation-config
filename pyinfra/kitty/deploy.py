b61d0b7306904ef82fbd13079291b69c6d9b62a2
apecnascimento <apecnascimento@gmail.com>
2024-05-03 15:59:24 -0400

    Add kitty config with pyinfra

---
+++ pyinfra/kitty/deploy.py
@@ -0,0 +1,26 @@
+from pyinfra import host
+from pyinfra.api import deploy
+from pyinfra.facts.server import User
+from pyinfra.operations import dnf, files
+
+
+@deploy("Install Kitty teminal")
+def deploy_kitty(_sudo=True):
+
+    dnf.packages(
+        name="Add kitty package",
+        present=True
+    )
+
+    files.directory(
+        name="Ensure kitty config folder",
+        path=f"/home/{host.get_fact(User)}/.config/kitty",
+        user=host.get_fact(User),
+        present=True
+    )
+
+    files.template(
+        name="Add kitty tokio night theme",
+        src="templates/kitty/kitty.conf.j2",
+        dest=f"/home/{host.get_fact(User)}/.config/kitty/kitty.conf"
+    )
